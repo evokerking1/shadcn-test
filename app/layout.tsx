57609880d2b40bb8270679afb4592ee94d983a5c
evokerking2 <165811512+evokerking2@users.noreply.github.com>
2025-01-14 14:57:21 +0000

    Updated stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,7 +39,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-          
+          <Toaster />
         {children}
       </body>
     </html>
