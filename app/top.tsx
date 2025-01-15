ba353bfa27312f4548f86c1175a9089ea17254ab
evokerking1 <cjpontius10@gmail.com>
2025-01-15 10:22:44 -0500

    Update top.tsx

--- app/top.tsx
+++ app/top.tsx
@@ -1,10 +1,10 @@
-import { ThemeProvider} from '@/components/theme-provider'
-import { ModeToggle } from '@/components/ui/theme_button'
-import { CommandDialogDemo } from '@/app/components/command'
+//import { ThemeProvider} from '@/components/theme-provider'
+//import { ModeToggle } from '@/components/ui/theme_button'
+//import { CommandDialogDemo } from '@/app/components/command'
 
 
-export default function TopBar() {
-    return(
-        <div className=""></div>
-    );
-}
+//export default function TopBar() {
+    //return(
+        //<div className=""></div>
+    //);
+//}
