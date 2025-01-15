5ffe462871b76b46df7761d9d84ef37f8154f2ee
evokerking2 <165811512+evokerking2@users.noreply.github.com>
2025-01-15 14:53:37 +0000

    Got a basic top bar working

--- app/top.tsx
+++ app/top.tsx
@@ -2,4 +2,9 @@ import { ThemeProvider} from '@/components/theme-provider'
 import { ModeToggle } from '@/components/ui/theme_button'
 import { CommandDialogDemo } from '@/app/components/command'
 
-return 
+
+export default function TopBar() {
+    return(
+        <div className=""></div>
+    );
+}
